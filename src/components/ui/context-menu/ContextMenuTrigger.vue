479e8c042072057e7a2b7f24336f38c30519d76f
Michael Lin <stargazer.taiwan@gmail.com>
2025-03-28 10:16:58 +0800

    added shadcn-vue@1.0.3

---
+++ src/components/ui/context-menu/ContextMenuTrigger.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ContextMenuTrigger, type ContextMenuTriggerProps, useForwardProps } from 'reka-ui'
+
+const props = defineProps<ContextMenuTriggerProps>()
+
+const forwardedProps: object = useForwardProps(props)
+</script>
+
+<template>
+  <ContextMenuTrigger v-bind="forwardedProps">
+    <slot />
+  </ContextMenuTrigger>
+</template>
---
+++ src/components/ui/sheet/Sheet.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { DialogRoot, type DialogRootEmits, type DialogRootProps, useForwardPropsEmits } from 'reka-ui'
+
+const props = defineProps<DialogRootProps>()
+const emits = defineEmits<DialogRootEmits>()
+
+const forwarded: object = useForwardPropsEmits(props, emits)
+</script>
+
+<template>
+  <DialogRoot v-bind="forwarded">
+    <slot />
+  </DialogRoot>
+</template>
