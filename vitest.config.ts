6113cd18908c3f0090e569dac75faa7de4270429
Michael Lin <stargazer.taiwan@gmail.com>
2026-04-14 11:01:29 +0800

    added vitest

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import { fileURLToPath } from 'node:url'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import viteConfig from './vite.config'
+
+// https://vite.dev/config/
+/** @type { import('vite').UserConfig } */
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'html', 'json', 'lcov'],
+        exclude: ['**/node_modules/**', '**/dist/**', '**/__tests__/**']
+      }
+    }
+  })
+)
